730348ddda2ac051fb9729d82e0275969b6f957a
krader <hiimkol@gmail.com>
2026-01-02 01:49:55 +0700

    feat: update inventory message for staging

--- inventory-service/src/main/java/com/example/simpleservice/HelloController.java
+++ inventory-service/src/main/java/com/example/simpleservice/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from Inventory Service";
+        return "Hello from Inventory Service (STAGING)";
     }
 }
